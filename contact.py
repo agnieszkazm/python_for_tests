360f5c9dc28b2d46717675aa79b2ce7edd8f4021
agnieszkazm <agnieszka.rydz@gmail.com>
2019-03-10 00:14:55 +0100

    assingment 3

---
+++ contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, first_name, midle_name, surmane, nickname, home_no, mobile, work, fax, mail, homepage):
+        self.first_name = first_name
+        self.midle_name = midle_name
+        self.surname = surmane
+        self.nickname = nickname
+        self.home_no = home_no
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.mail = mail
+        self.homepage = homepage
--- test_add_group.py
+++ test_add_group.py
@@ -2,19 +2,12 @@
 from selenium import webdriver
 import unittest
 from group import Group
+from contact import Contact
 
 class Test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
-
-    def test_add_group(self):
-        wd = self.wd
-        self.open_website(wd)
-        self.login(wd, "admin", "secret")
-        self.create_group(wd, Group("test_name", "test_header", "test_footer"))
-        self.return_to_group_page(wd)
-        self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
@@ -40,6 +33,40 @@ class Test_add_group(unittest.TestCase):
         # submit
         wd.find_element_by_name("submit").click()
 
+    def create_new_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.midle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.surname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_no)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.mail)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        wd.find_element_by_name("submit").click()
+
     def login(self, wd, name, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -50,6 +77,21 @@ class Test_add_group(unittest.TestCase):
 
     def open_website(self, wd):
         wd.get("http://localhost/addressbook/index.php")
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_website(wd)
+        self.login(wd, "admin", "secret")
+        self.create_group(wd, Group("test_name", "test_header", "test_footer"))
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_website(wd)
+        self.login(wd, "admin", "secret")
+        self.create_new_contact(wd, Contact("Testname", "Testname2", "Testlastname", "test", "555", "666", "777", "888", "test", "www.example.com"))
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
